02714421070876ca648e31a34c2fc4014f455c0e
tanishkadaramoni8 <75026809+tanishkadaramoni8@users.noreply.github.com>
2021-05-04 13:45:27 +0530

    Add files via upload

---
+++ lipstik.js
@@ -0,0 +1,25 @@
+function change()
+{
+    window.location.replace("index.html");
+}
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('my_filter_image.png');
+}
---
+++ main.js
@@ -0,0 +1,25 @@
+function move()
+{
+    window.location.replace("lipstik.html");
+}
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('my_filter_image.png');
+}
